75e7a64add60a97dbf21630b18f4f6f9e0083626
Anar <anarabdiyev444@gmail.com>
2017-05-26 12:25:04 +0400

    Add files via upload

---
+++ Anar.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp10
+{
+    public partial class Form1 : Form
+    {
+        Panel[] pan = new Panel[5];
+        Label[] lb = new Label[5];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+     
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int col = 10;
+            int row = 10;
+            for (int t = 1; t < row; t++)
+            {
+
+                for (int i = 1; i < col; i++)
+                {
+
+                    Button Btn = new Button();
+                    Btn.Text = (row * t+i+1).ToString();
+                    Btn.Width = 64;
+                    Btn.Height = 23;
+                    Btn.Location = new Point(t*Btn.Width,i*Btn.Height);
+                    this.Controls.Add(Btn);
+
+                }
+            }
+        }
+    }
+}
+
+
+       
+
+
